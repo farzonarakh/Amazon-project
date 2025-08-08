2436eb4d340b50d61d7047365714d6254a169188
FarzonaR <farzonarakh2@gmail.com>
2025-08-08 23:13:33 +0500

    Create class version of cart

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,9 +1,14 @@
-function Cart(localStorageKey) {
-  const cart = {
-  cartItems: undefined,
+class Cart{
+  cartItems;
+  localStorageKey;
+
+  constructor(localStorageKey) {
+    this.localStorageKey = localStorageKey;
+    this.loadFromStorage();
+  }
 
   loadFromStorage() {
-    this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
 
     if (!this.cartItems) {
       this.cartItems = [{
@@ -18,11 +23,11 @@ function Cart(localStorageKey) {
         deliveryOptionId: '2'
       }];
     }
-  },
+  }
 
   saveToStorage() {
-    localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems));
-  },
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
 
   addToCart(productId,quantity) {
     let matchingItem;
@@ -44,7 +49,7 @@ function Cart(localStorageKey) {
     }
 
     this.saveToStorage();
-  },
+  }
 
   removeFromCart(productId) {
     const newCart = [];
@@ -58,7 +63,7 @@ function Cart(localStorageKey) {
     this.cartItems = newCart;
 
     this.saveToStorage();
-  },
+  }
 
   calculateCartQuantity(link) {
     let cartQuantity = 0;
@@ -77,7 +82,7 @@ function Cart(localStorageKey) {
       document.querySelector(link)
       .innerHTML = `Items(${cartQuantity}):`;
     }
-  },
+  }
 
   updateQuantity(productId, newQuantity) {
     let matchingItem;
@@ -91,7 +96,7 @@ function Cart(localStorageKey) {
     matchingItem.quantity = newQuantity;
     
     this.saveToStorage();
-  },
+  }
 
   updateDeliveryOption(productId, deliveryOptionId) {
     let matchingItem;
@@ -106,18 +111,13 @@ function Cart(localStorageKey) {
 
     this.saveToStorage();
   }
-  };
-
-  return cart;
 }
 
-const cart = Cart('cart-oop');
-const businessCart = Cart('cart-business');
-
-cart.loadFromStorage();
-
-businessCart.loadFromStorage();
+  
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business');
 
 console.log(cart);
 console.log(businessCart);
 
+console.log(businessCart instanceof Cart);
