0a76299e01751b3929e929a6c006f7f395ade2f8
FarzonaR <farzonarakh2@gmail.com>
2025-08-09 12:10:10 +0500

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -55,6 +55,18 @@ class Clothing extends Product{
   }
 }
 
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+/*
+function logThis() {
+  console.log(this);
+}
+logThis.call('hello');
+*/
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
